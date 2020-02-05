c18c18ae777aa6193e736a2955109516f55d2464
Widka <Weedka13@gmail.com>
2020-02-05 16:15:13 +0100

    Debut projet Fleuriste

---
+++ Jour6/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img class="logo" src="assets/img/logo.png" alt="">
+        </div>
+        <h1>UN TITRE DE FLEURS</h1>
+        <nav>
+            <a href="index.php">Accueil</a>
+            <a href="fleurs.php">Nos Fleurs</a>
+            <a href="contact.php">Contactez-nous</a>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        
+    </footer>
+</body>
+</html>
