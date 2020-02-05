7d8c58dd8c0e1e70406c5a445f6dd0c0cea31c73
Widka <Weedka13@gmail.com>
2020-02-05 17:10:26 +0100

    FIn de journée

---
+++ Jour6/php/view/section-contact.php
@@ -0,0 +1,5 @@
+<?php
+    require_once "php/view/index.php";
+    require_once "php/view/fleurs.php";
+    require_once "php/view/contact.php"
+?>
---
+++ Jour6/php/view/section-index.php
@@ -0,0 +1,22 @@
+<section>
+        <h2>Nos types de Bouquets</h2>
+        <img src="assets/img/code1.jpeg" alt="">
+        <div class="ligne">
+            <div class="colonne">
+                    <p>Texte 1</p>
+                </br>
+                <img src="assets/img/Fleur1.jpeg">
+            </div>
+            <div class="colonne">
+                    <p>Texte 2</p>
+                </br>
+                    <img src="assets/img/Fleur2.jpeg">
+            </div>
+            <div class="colonne">
+                    <p>Texte 3</p>
+                </br>
+                    <img src="assets/img/Fleur3.jpeg">
+            </div>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas, facere, optio alias dolor officiis obcaecati nesciunt error odio quibusdam dicta hic! Quae eveniet fugiat quasi doloremque. Eum quis nam minus.</p>
+    </section>
