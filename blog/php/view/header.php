32aaed39567a79acb77d2b022a1070480b17e600
Widka <Weedka13@gmail.com>
2020-03-03 17:12:20 +0100

    rajout

--- blog/php/view/header.php
+++ blog/php/view/header.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Blog d'actus</title>
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
+    </head>
 <body>
     <header> 
     <div class="toggle_btn">
