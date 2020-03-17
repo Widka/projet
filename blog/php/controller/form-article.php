6a8c7c6f6d0544d6011d7392ba89abb5a1f38c48
Widka <Weedka13@gmail.com>
2020-03-17 11:00:22 +0100

    Test push

--- blog/php/controller/form-article.php
+++ blog/php/controller/form-article.php
@@ -1,15 +1,30 @@
 <?php
 
-if(count($_REQUEST) > 0)
+function filtrer($name="id")
 {
-$tabAssoColonneValeur = [
-    "titre" => $_REQUEST["titre"],
-    "contenu" => $_REQUEST["contenu"],
-    "image" => $_REQUEST["image"],
-    "datePublication" => $_REQUEST["datePublication"],
-    "categorie" => $_REQUEST["categorie"],
-];
+    $resultat = $_REQUEST[$name] ?? "";
+    return $resultat;
+}
 
+$identifiantFormulaire = filtrer("identifiantFormulaire");
+
+if ($identifiantFormulaire == "create")
+{
+    $tabAssoColonneValeur = [
+        "titre"            => filtrer("titre"),
+        "contenu"          => filtrer("contenu"),
+        "image"            => filtrer("image"),
+        "datePublication"  => filtrer("datePublication"),
+        "categorie"        => filtrer("categorie"),
+    ];
+    extract($tabAssoColonneValeur);
+
+    if ($titre != "" 
+            && $contenu != ""
+            && $image != ""
+            && $datePublication != ""
+            && $categorie != "")
+    {
     $requeteSQL =
 <<<CODESQL
 
@@ -20,7 +35,92 @@ VALUES
 
 CODESQL;
 
-require_once "php/model/envoyer-sql.php";
+    require_once "php/model/envoyer-sql.php";
 
-echo "Votre article à bien été publié !($requeteSQL)";
+    echo "Votre article à bien été publié !($requeteSQL)";
+    }
+    else
+    {
+        echo "Veuillez remplir tous les champs obligatoires";
+    }
+}
+if ($identifiantFormulaire == "delete")
+{
+    
+    $tabAssoColonneValeur = [
+        "id"            => filtrer("id"),
+    ];
+    extract($tabAssoColonneValeur);
+    
+    $id = intval($id);
+
+    if ($id > 0)
+    {
+        
+        $requeteSQL   =
+<<<CODESQL
+
+DELETE FROM articles
+WHERE id = :id
+
+CODESQL;
+
+        require_once "php/model/envoyer-sql.php";
+
+       
+        echo "Votre article a bien été supprimé ($requeteSQL)";
+
+    }
+    else
+    {
+        
+        echo "Merci de ne pas sombrer du coté obscur";
+    }
+
+}
+if ($identifiantFormulaire == "update")
+{
+    $tabAssoColonneValeur = [
+        "id"               => filtrer("id"),
+        "titre"            => filtrer("titre"),
+        "contenu"          => filtrer("contenu"),
+        "image"            => filtrer("image"),
+        "datePublication"  => filtrer("datePublication"),
+        "categorie"        => filtrer("categorie"),
+    ];
+    
+    extract($tabAssoColonneValeur);
+    
+    if ($id != ""                   
+        && $titre != "" 
+        && $contenu != ""
+        && $image != ""
+        && $datePublication != ""
+        && $categorie != "")
+    {
+        $requeteSQL   =
+<<<CODESQL
+
+UPDATE articles 
+SET 
+    titre           = :titre,
+    contenu         = :contenu,
+    image           = :image,
+    datePublication = :datePublication,
+    categorie       = :categorie
+WHERE 
+    id = :id;
+
+
+CODESQL;
+
+
+    
+        require_once "php/model/envoyer-sql.php";
+
+        
+        echo "Votre article à bien été modifié ($requeteSQL)";
+
+    }
+
 }
--- blog/php/view/section-admin.php
+++ blog/php/view/section-admin.php
@@ -1,14 +1,215 @@
 <section>
     <h2>Formulaire de création d'Articles</h2>
-    <input type="text" name="titre" required placeholder="Entrez le titre">
-    <textarea name="contenu" cols="60" rows="8" required placeholder="Entrez le contenu"></textarea>
-    <input type="text" name="image" required value="assets/img/photo1.jpg">
-    <input type="datetime" name="datePublication" id="datePublication">
-    <input type="text" name="catégorie" required placeholder="Entrez la catégorie">
-    <button type="submit">Publier l'article</button>
-    <div class="confirmation">
-    <?php
-    require_once "php/controller/form-article.php"
-    ?>
-    </div>
+    <form id="create" class="admin" action="" method="POST">
+        <input type="text" name="titre" required placeholder="Entrez le titre">
+        <textarea name="contenu" cols="60" rows="8" required placeholder="Entrez le contenu"></textarea>
+        <input type="text" name="image" required value="assets/img/photo1.jpg">
+        <input type="text" name="datePublication" value="<?php echo date("Y-m-d H:i:s") ?>">
+        <input type="text" name="catégorie" required placeholder="Entrez la catégorie">
+        
+        <input type="hidden" name="identifiantFormulaire" value="create">
+
+        <button type="submit">Publier l'article</button>
+        <div class="confirmation">
+            <?php
+$identifiantFormulaire = $_REQUEST["identifiantFormulaire"] ?? "";
+if ($identifiantFormulaire == "create")
+{
+    require "php/controller/form-articles.php";
+}
+            ?>
+        </div>
 </section>
+<section class="cache">
+    <h2>Formulaire pour suppression d'Articles(Delete)</h2>
+    <form id="delete" action="" method="POST">
+        <input type="text" name="id" required placeholder="entrez id">
+        
+        <input type="hidden" name="identifiantFormulaire" value="delete">
+        <button>Supprimer l'article</button>
+        <div class="confirmation">
+        <?php 
+$identifiantFormulaire = $_REQUEST["identifiantFormulaire"] ?? "";
+if ($identifiantFormulaire == "delete")
+{
+    require "php/controller/form-articles.php"; 
+}        
+            ?>
+        </div>
+    </form>    
+</section>
+<section class="updateSection cache">
+    <button class="closePopup">Fermer la popup</button>
+    <h2>Formulaire de modification d'article (Update)</h2>
+    <form id="update" class="admin" action="" method="POST">
+        <div class="infosUpdate">
+            <input type="text" name="titre" required placeholder="entrer le titre">
+            <textarea name="contenu" cols="60" rows="8" required placeholder="entrer le contenu"></textarea>
+            <input type="text" name="image" required value="assets/img/photo1.jpg">
+            <input type="text" name="datePublication" value="<?php echo date("Y-m-d H:i:s") ?>">
+            <input type="text" name="categorie" required placeholder="entrez la catégorie">
+            <input type="text" name="id" required placeholder="entrez id ligne">
+        </div>
+
+        
+        <input type="hidden" name="identifiantFormulaire" value="update">
+        <button type="submit">Modifier l'article</button>
+        <div class="confirmation">
+        <?php 
+$identifiantFormulaire = $_REQUEST["identifiantFormulaire"] ?? "";
+if ($identifiantFormulaire == "update")
+{
+    require "php/controller/form-articles.php"; 
+}        
+            ?>
+        </div>
+
+    </form>
+</section>
+<section>
+    <h2>LISTE DES ARTICLES</h2>
+    <table>
+        <thead>
+            <tr>
+                <td>id</td>
+                <td>titre</td>
+                <td>contenu</td>
+                <td>image</td>
+                <td>categorie</td>
+                <td>modifier</td>
+                <td>supprimer</td>
+            </tr>
+        </thead>
+        <tbody>
+            
+            <?php
+
+$requeteSQL =
+<<<CODESQL
+
+SELECT * FROM `articles`
+ORDER BY datePublication DESC
+
+CODESQL;
+
+
+$tabAssoColonneValeur = [];
+
+
+require "php/model/envoyer-sql.php";
+
+
+$tabLigne = $pdoStatement->fetchAll();
+
+
+foreach($tabLigne as $tabAsso)
+{
+    extract($tabAsso); 
+
+    echo
+<<<CODEHTML
+
+<tr>
+    <td>$id</td>
+    <td>$titre</td>
+    <td>$contenu</td>
+    <td>$image</td>
+    <td>$categorie</td>
+    <td>
+        <button data-id="$id" class="update">Modifier</button>
+        
+        <div class="infosUpdate cache">
+            <input type="text" name="titre" required placeholder="entrer le titre" value="$titre">
+            <textarea name="contenu" cols="60" rows="8" required placeholder="entrer le contenu">$contenu</textarea>
+            
+            <input type="text" name="image" required value="$image">
+            
+            <input type="text" name="datePublication" value="$datePublication">
+            <input type="text" name="categorie" required placeholder="entrez la catégorie" value="$categorie">
+            
+            <input type="text" name="id" required placeholder="entrez id ligne" value="$id">
+        </div>
+
+    </td>  
+    <td><button data-id="$id" class="delete">Supprimer</button></td>  
+</tr>
+
+CODEHTML;
+}
+
+
+?>
+        </tbody>
+    </table>
+</section>
+
+
+<script>
+var boutonClose = document.querySelector("button.closePopup");
+boutonClose.addEventListener("click", function(){
+    var baliseSectionUpdate = document.querySelector("section.updateSection");
+    baliseSectionUpdate.classList.add("cache");
+});
+
+
+var listeBoutonUpdate = document.querySelectorAll("button.update");
+
+listeBoutonUpdate.forEach(function(bouton){
+    
+    bouton.addEventListener("click", function(event){
+        
+        console.log("Click sur le bouton modifier");
+        
+        var baliseBouton = event.target;
+        var baliseTd = baliseBouton.parentNode;
+        var baliseUpdate = baliseTd.querySelector(".infosUpdate");
+
+       
+        console.log(baliseBouton);
+        console.log(baliseTd);
+        console.log(baliseUpdate);
+
+        var baliseUpdateForm = document.querySelector("form#update div.infosUpdate");
+        
+        baliseUpdateForm.innerHTML = baliseUpdate.innerHTML;
+
+        
+        var baliseSection = document.querySelector(".updateSection");
+        baliseSection.classList.remove("cache"); 
+    });
+
+});
+    
+var listeBoutonDelete = document.querySelectorAll("button.delete");
+
+listeBoutonDelete.forEach(function(bouton){
+    
+    bouton.addEventListener("click", function(event){
+        console.log("Tu as clické");
+        
+        var idBouton = event.target.getAttribute("data-id");
+        console.log(idBouton);
+        
+        var champId = document.querySelector("form#delete input[name=id]");
+        
+        champId.value = idBouton;
+
+        
+        var confirmation = window.confirm("Est-tu sûr ?");
+        if (confirmation)
+        {
+            
+            var formDelete = document.querySelector("form#delete");
+            
+            formDelete.submit();
+        }
+        else
+        {
+          
+        }
+    });
+
+});
+
+
+</script>
