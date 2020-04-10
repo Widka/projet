1c1426e5a6990fae88599c50ff8f413e02e14330
Widka <Weedka13@gmail.com>
2020-04-10 12:00:16 +0200

    push projet solo

--- projet-solo/espace-membre/includes/database.php
+++ projet-solo/espace-membre/includes/database.php
@@ -17,8 +17,11 @@ class Database {
             'password' => ''
         );
     }
+
+    /* Connexion a la BDD */ 
+
     // je crée une méthode qui va me permettre de géré mon PDO
-    private function getPDO() {
+    public function getPDO() {
         // je crée ma connexion a la BDD
         try {
             // je vérifie si elle n'est pas déja connecté
@@ -27,9 +30,31 @@ class Database {
                 $pdo = new PDO('mysql:dbname=' . $this->tokenAuth['dbname'] . ';host=' . $this->tokenAuth['host'], $this->tokenAuth['user'], $this->tokenAuth['password']);
                 // ensuite je définis 2 attributs : ERRMODE et ERRMODE_EXCEPTION
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)
+                $this->pdo = $pdo;
             }
         } catch (PDOexception $e){
             var_dump($e);
-        }
+        }    
     }
+        /* Fermer la connexion a la BDD */
+        private function shutdown() {
+            $this->pdo = null;
+            return true;
+        }
+    /* Je crée les différentes méthodes pour me permettre d'éxécuter mes requêtes */
+    
+    // Requête query a la BDD
+    public function query($statement) {
+        $request = $this->getPDO()->query($statement);
+        $this->shutdown();
+        return $request;
+    }   
+    // Requête prepare a la BDD
+    public function prepare($statement, $values) {
+        $request = $this->getPDO()->prepare($statement);
+        $request->execute($values);
+        $this->shutdown();
+        return $request;
+    }
+
 }
--- projet-solo/espace-membre/register.php
+++ projet-solo/espace-membre/register.php
@@ -1,3 +1,13 @@
+<?php
+
+session_start();
+include 'includes/database.php';
+
+if(isset($_POST['submit'])){
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -14,20 +24,28 @@
         </div>
         <div class="form-div text-center">
             <h3>Inscription</h3>
-            <form>
+            <?php 
+                if(isset($errorMessage)) {
+            ?> 
+            <p style="color: red;"><?= $errorMessage ?></p> <?php } ?>
+            <?php 
+                if(isset($successMessage)) {
+            ?> 
+            <p style="color: green;"><?= $successMessage ?></p> <?php } ?>
+            <form method="post" action="">
                 <span>Pseudo :</span><br>
-                <input type="name" placeholder="Pseudo"><br>
+                <input type="text" name="pseudo" placeholder="Pseudo"><br>
 
                 <span>Adresse Mail :</span><br>
-                <input type="name" placeholder="Adresse Mail"><br>
+                <input type="email" name="email" placeholder="Adresse Mail"><br>
 
                 <span>Mot de passe :</span><br>
-                <input type="name" placeholder="Mot de passe"><br>
+                <input type="password" name="password" placeholder="Mot de passe"><br>
 
                 <span>Confirmation mot de passe :</span><br>
-                <input type="name" placeholder="Mot de passe"><br><br>
+                <input type="password" name="password_confirm" placeholder="Mot de passe"><br><br>
 
-                <input type="submit" value="Submit">
+                <input type="submit" name="submit" value="Submit">
             </form>
         </div>
     </body>
